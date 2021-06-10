577ddf807e96eeb8fd28bd9f068c110d099f898a
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-06-10 14:48:41 -0500

    Create matplotlib_basicPieChart.py

---
+++ matplotlib_basicPieChart.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+slices = [120, 80, 30, 20]
+labels = ['Sixty', 'Forty', 'Extra1', 'Extra2']
+colors = ['#008fd5', '#fc4f30', '#e5ae37', '#6d904f']
+
+# this is to output or create the pie chart
+plt.pie(slices, labels=labels, colors=colors, wedgeprops={'edgecolor': 'black'})
+
+plt.title('My Awesome Pie Chart')
+plt.tight_layout()
+plt.show()
